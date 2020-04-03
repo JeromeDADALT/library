4c6afe6c963f8b1c8ae5ecb68da30b4c05a16a03
Jérôme DA DALT <jerome.dadalt@lapiscine.pro>
2020-04-03 10:56:38 +0200

    installation et utilisation sass

---
+++ public/src/js/app.js
@@ -0,0 +1,10 @@
+// pour tester le bon fonctionnement de Jquery
+// console.log('Hello world');
+
+
+//je sélectionne le document, j'exécute que si ma page est bien chargée avant
+$(document).ready(function() {
+    //je sélectionne avec jQuery tous les éléments qui ont la classe "livres"
+    console.log($('.livres'));
+});
+
